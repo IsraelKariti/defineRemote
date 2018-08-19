aad219c2b55c58124776230c956d20cec145bc65
aa <mrkarity@gmail.com>
2018-08-19 18:27:10 +0300

    USA-RULES to 3000000

--- app/src/main/java/com/example/izi/gittutorial/MainActivity.java
+++ app/src/main/java/com/example/izi/gittutorial/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
         int commitLocal = 1;
         int commitGlobal = 2;
         x = 800;
-        int USARULES = 3000;
+        int USARULES = 3000000;
     }
 }
