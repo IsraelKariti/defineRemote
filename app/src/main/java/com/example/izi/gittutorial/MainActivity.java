b6b7fedbcadab071ed9418d47584d81767434eb0
aa <mrkarity@gmail.com>
2018-08-19 14:04:36 +0300

    united

--- app/src/main/java/com/example/izi/gittutorial/MainActivity.java
+++ app/src/main/java/com/example/izi/gittutorial/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         int z = -5000;
         int t = 0;
         int cloud = 100;
-
         int commitLocal = 1;
         int commitGlobal = 2;
+        x = 800;
     }
 }
